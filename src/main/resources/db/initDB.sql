e5060c79211e8ecd69aa4e9c4d03ff42cfa14f18
YuryMazniou <tantr@tut.by>
2019-09-20 12:15:12 +0300

    fix for travis

--- src/main/resources/db/initDB.sql
+++ src/main/resources/db/initDB.sql
@@ -5,18 +5,18 @@ DROP TABLE IF EXISTS persons;
 CREATE TABLE persons
 (
   id               INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
-  firstname        VARCHAR (100)          NOT NULL,
-  lastname         VARCHAR  (100)         NOT NULL,
-  patronymic       VARCHAR (100)          NOT NULL,
-  email            VARCHAR (100)          NOT NULL,
-  password         VARCHAR  (100)         NOT NULL
+  firstname        VARCHAR (100)   CHARACTER SET utf8 COLLATE utf8_general_ci       NOT NULL,
+  lastname         VARCHAR  (100)   CHARACTER SET utf8 COLLATE utf8_general_ci      NOT NULL,
+  patronymic       VARCHAR (100)   CHARACTER SET utf8 COLLATE utf8_general_ci       NOT NULL,
+  email            VARCHAR (100)   CHARACTER SET utf8 COLLATE utf8_general_ci       NOT NULL,
+  password         VARCHAR  (100)   CHARACTER SET utf8 COLLATE utf8_general_ci      NOT NULL
 );
 CREATE UNIQUE INDEX users_unique_email_idx ON persons (email);
 
 
 CREATE TABLE tasks (
   id          INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
-  name        VARCHAR (100)   NOT NULL,
+  name        VARCHAR (100) CHARACTER SET utf8 COLLATE utf8_general_ci   NOT NULL,
   person_id   INT            NOT NULL,
   state       BOOLEAN        NOT NULL,
   FOREIGN KEY (person_id) REFERENCES persons (id) ON DELETE CASCADE
@@ -26,6 +26,6 @@ CREATE TABLE log_of_task(
   id              INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
   task_id         INT         NOT NULL,
   spent_of_time   BIGINT      NOT NULL,
-  comment         TEXT        NOT NULL,
+  comment         TEXT  CHARACTER SET utf8 COLLATE utf8_general_ci      NOT NULL,
   FOREIGN KEY (task_id ) REFERENCES tasks (id) ON DELETE CASCADE
 );
